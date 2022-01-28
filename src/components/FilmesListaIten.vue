01729962927802b51646c22b3f9706cbb4cbabac
Matheus <matheususko@gmail.com>
2022-01-28 16:12:30 -0300

    Segundo Commit

---
+++ src/components/FilmesListaIten.vue
@@ -0,0 +1,27 @@
+<template>
+<li class="list-group-item">
+        <span>{{ filme.titulo }} | {{ filme.ano }}</span>
+        <button v-on:click="selecionar" class="btn btn-secondary float-right">Selecionar</button>
+    </li>
+</template>
+
+<script>
+
+import { eventBus } from './../main'
+
+export default {
+    props: {
+        filme: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        selecionar() {
+            // this.$emit('selecionarFilme', this.filme)
+            // eventBus.$emit('selecionarFilme', this.filme)
+            eventBus.selecionarFilme(this.filme)
+        }
+    }
+}
+</script>
